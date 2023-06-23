e2280fb631f4b1f2ff3d5b208601979740824d8d
Sid <98279769+Siddhantiscoding@users.noreply.github.com>
2023-06-23 21:01:50 +0530

    Add files via upload

---
+++ Code/RGBTOGRAY.py
@@ -0,0 +1,60 @@
+# Import necessary libraries
+import sys
+
+import cv2
+import os
+from datetime import datetime
+import os
+import time
+from PIL import Image
+
+
+def capturing():
+    path = r'/home/ysapi/Desktop/ProjectPython'
+    os.chdir(path)
+    i = 1
+    wait = 0
+    video = cv2.VideoCapture(0)
+
+    while True:
+        # Read video by read() function and it
+        # will extract and return the frame
+        ret, img = video.read()
+
+        # Put current DateTime on each frame
+        font = cv2.FONT_HERSHEY_PLAIN
+        cv2.putText(img, str(datetime.now()), (20, 40),
+                    font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Display the image
+        cv2.imshow('live video', img)
+
+        # wait for user to press any key
+        key = cv2.waitKey(100)
+
+        # wait variable is to calculate waiting time
+        wait = wait + 100
+
+        if key == ord('q'):
+            break
+        # when it reaches to 5000 milliseconds
+        # we will save that frame in given folder
+        if wait == 5000:
+            filename = 'Frame_' + str(i) + '.jpg'
+
+            # Save the images in given path
+            cv2.imwrite(filename, img)
+            i = i + 1
+            wait = 0
+            return
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
